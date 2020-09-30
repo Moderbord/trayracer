3ff4d5dbbca2cba85055767e252e5f5a83dfda58
Pontus Andersson <pontande@gmail.com>
2020-09-30 17:06:22 +0200

    First step completed

--- code/main.cc
+++ code/main.cc
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include "window.h"
+#include <chrono>
+//#include "window.h"
 #include "vec3.h"
 #include "raytracer.h"
 #include "sphere.h"
@@ -11,28 +12,16 @@
 #define degtorad(angle) angle * MPI / 180
 
 unsigned int width = 200;
-unsigned int height = 100;
+unsigned int height = 150;
 unsigned int raysPerPixel = 1;
 unsigned int maxBounces = 5;
 bool verbose = false;
-bool outputToFile = false;
-std::string filepath = "../out/image";
+char* filepath;
 
 int render()
 {
-    if (verbose)
-    {
-        //if (outputToFile)
-        printf("Rendering to %s with following settings:\n", filepath.c_str());
-
-        printf("Image dimensions: %ux%u\n", width, height);
-        printf("Rays per pixel: %u\n", raysPerPixel);
-        printf("Max bounces per ray: %u\n\n", maxBounces);
-    }
-
     std::vector<Color> framebuffer;
     framebuffer.resize(width * height);
-
     Raytracer rt = Raytracer(width, height, framebuffer, raysPerPixel, maxBounces);
 
     // Create some objects
@@ -79,18 +68,60 @@ int render()
     test = new Sphere(1, { 4,1, 0 }, mat);
     rt.AddObject(test);
 
-
     mat4 view { 1,  0,  0,  0,
                 0,  1,  0,  0,
                 0,  0,  1,  0,
                 0,  1,  10,  1 };
     rt.SetViewMatrix(view);
+
+    // Timepoint 1
+    std::chrono::high_resolution_clock::time_point t0 = std::chrono::high_resolution_clock::now();
+
+    // Render
     rt.Raytrace();
 
+    // Timepoint 2
+    std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = t1 - t0;
+    // Time
+    size_t numRays = width * height * raysPerPixel;
+    double numMRays = (double)numRays / 1000000;
+    double mRaysSec = numMRays / duration.count();
+    printf("Completed in [%f] seconds, with a total of [%zu] rays, [%f] MRays/s \n", duration.count(), numRays, mRaysSec);
 
-    
-    //stbi_write_jpg(filepath.c_str(), width, height, 3, (float*)&framebuffer[0], 100);
-    stbi_write_bmp(filepath.c_str(), width, height, 3, (float*)&framebuffer[0]);
+    // Export to file
+    if (filepath != nullptr)
+    {
+        uint8_t* pixels = new uint8_t[width * height * 3]; // Initiate pixel buffer
+
+        size_t index = 0;
+        for (size_t i = 0; i < width * height; i++) 
+        {
+            pixels[index++] = 255.0f * framebuffer[i].r;
+            pixels[index++] = 255.0f * framebuffer[i].g;
+            pixels[index++] = 255.0f * framebuffer[i].b;
+        }
+        
+        stbi_flip_vertically_on_write(1);
+
+        if(stbi_write_png(filepath, width, height, 3, pixels, width * 3) == 0)
+        {
+            fprintf(stderr, "Failed to write to file %s\n", filepath);
+        }
+        else
+        {
+            printf("Wrote to file %s\n", filepath);
+        }
+        delete[] pixels;
+        pixels = nullptr;
+    }
+
+    if (verbose)
+    {
+        printf("Image dimensions: %ux%u\n", width, height);
+        printf("Rays per pixel: %u\n", raysPerPixel);
+        printf("Max bounces per ray: %u\n", maxBounces);
+    }
 
     return 0;
 }
@@ -99,7 +130,7 @@ int main(int argc, char *argv[])
 {
     int opt = 0;
 
-    while ((opt = getopt(argc, argv, "vs:o:r:b:")) != -1)
+    while ((opt = getopt(argc, argv, "vs:o:r:b:t")) != -1)
     {
         switch(opt) 
         {
@@ -118,7 +149,6 @@ int main(int argc, char *argv[])
                 break;
 
             case 'o': // Output to file
-                outputToFile = true;
                 filepath = optarg;
                 break;
 
@@ -130,12 +160,20 @@ int main(int argc, char *argv[])
                 maxBounces = atoi(optarg);
                 break;
 
+            case 't': // Test setting
+                printf("Tracing using pre-defined test settings..\n");
+                width = 1000;
+                height = 500;
+                raysPerPixel = 20;
+                maxBounces = 5;
+                break;
+
             case '?':
                 if (optopt == 's') 
                     printf("\nSpecify size of rendered image, <width>x<height>\n");
 
                 else if (optopt == 'o') 
-                    printf("\nSpecify filepath and filename\n");
+                    printf("\nSpecify filepath\n");
 
                 else if (optopt == 'r') 
                     printf("\nSpecify number of rays per pixel\n");
